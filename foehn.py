74d764db8c4bc70f06fa3e3a002576847d3bb477
JackLeEmmerdeur <jackleemmerdeur@googlemail.com>
2019-03-16 05:24:55 +0100

    Testing deamonize library

--- foehn.py
+++ foehn.py
@@ -10,6 +10,8 @@ from src.classes.FanThread import FanThread
 from src.lib.helpers import file_exists, is_string, \
 	is_sequence, is_dict, is_integer
 
+# from daemonize import Daemonize
+
 exited_via_ctrl_c = False
 
 globalfan = None
@@ -76,56 +78,56 @@ def signal_handler(sig, frame):
 signal.signal(signal.SIGINT, signal_handler)
 
 
-@click.command()
-@click.option(
-	"--temp_tresholds", type=click.STRING, default="[54,59,74,80]", required=False,
-	help=
-	"Variable length list of temperature thresholds. Use e.g. [50,60,70,80] "
-	"for spinning at 0% speed from 0-50deg, 33% from 50-60deg, 66% from "
-	"60-70deg and 100% from 70-80deg. Without static_speed the fan will "
-	"spread at relative procentage.")
-@click.option(
-	"--interval", type=click.IntRange(2, 20), default=5, required=False,
-	help="Measure interval (default 5 secs)")
-@click.option(
-	"--freq", type=click.IntRange(2, 300), default=25, required=False,
-	help="PWM Frequency (default 25)")
-@click.option(
-	"--pwmgpio", type=click.IntRange(-1, 27), required=False, default=-1,
-	help="PWM-Output GPIO-Port (default 25)")
-@click.option(
-	"--constant_speed", type=click.IntRange(0, 100), default=None, required=False,
-	help=
-	"Sets the fans dutycycle to a static percentage on every range from the "
-	"above temp_threshold list.")
-@click.option(
-	"--variable_speeds", type=click.STRING, default=None, required=False,
-	help=
-	"Sets individual fan speeds for every temp_threshold passed. "
-	"Has to correspond with the temp_thresholds-argument, "
-	"e.g. temp_thresholds='[45,55,70,80] and variable_speeds='[20,30,50,90]' "
-	"means spin at 0% speed at 0-45deg, 20% at 45-55deg, 30% at 55-70, "
-	"50% at 70-80deg and 90% when over 80deg the")
-@click.option(
-	"--fanstartlevel", type=click.INT, default=1, required=False,
-	help=
-	"The level at which to start the fan. If e.g [45,50,60,70,80] was "
-	"passed to temp_thresholds a stoplevel of 2 will indicate that the "
-	"fan has to start when 60deg is reached")
-@click.option(
-	"--fancooldown", type=click.STRING, default="10sek", required=False,
-	help=
-	"Either a number-string, e.g. '10' which means to cool for another 10 "
-	"interval turns after temperature dropping below fanstartlevel or a "
-	"time-string, e.g. '10sec' or '10min' means cool for another "
-	"10 seconds or 10 minutes"
-)
-@click.option(
-	"--debug", type=click.BOOL, default=False, required=False,
-	help="Print debug messages (1 or 0, true or false)")
-@click.option(
-	"--fan/--nofan", is_flag=True, default=True,
-	help="Use the fan (--fan) or don't (--nofan)")
+# @click.command()
+# @click.option(
+# 	"--temp_tresholds", type=click.STRING, default="[54,59,74,80]", required=False,
+# 	help=
+# 	"Variable length list of temperature thresholds. Use e.g. [50,60,70,80] "
+# 	"for spinning at 0% speed from 0-50deg, 33% from 50-60deg, 66% from "
+# 	"60-70deg and 100% from 70-80deg. Without static_speed the fan will "
+# 	"spread at relative procentage.")
+# @click.option(
+# 	"--interval", type=click.IntRange(2, 20), default=5, required=False,
+# 	help="Measure interval (default 5 secs)")
+# @click.option(
+# 	"--freq", type=click.IntRange(2, 300), default=25, required=False,
+# 	help="PWM Frequency (default 25)")
+# @click.option(
+# 	"--pwmgpio", type=click.IntRange(-1, 27), required=False, default=-1,
+# 	help="PWM-Output GPIO-Port (default 25)")
+# @click.option(
+# 	"--constant_speed", type=click.IntRange(0, 100), default=None, required=False,
+# 	help=
+# 	"Sets the fans dutycycle to a static percentage on every range from the "
+# 	"above temp_threshold list.")
+# @click.option(
+# 	"--variable_speeds", type=click.STRING, default=None, required=False,
+# 	help=
+# 	"Sets individual fan speeds for every temp_threshold passed. "
+# 	"Has to correspond with the temp_thresholds-argument, "
+# 	"e.g. temp_thresholds='[45,55,70,80] and variable_speeds='[20,30,50,90]' "
+# 	"means spin at 0% speed at 0-45deg, 20% at 45-55deg, 30% at 55-70, "
+# 	"50% at 70-80deg and 90% when over 80deg the")
+# @click.option(
+# 	"--fanstartlevel", type=click.INT, default=1, required=False,
+# 	help=
+# 	"The level at which to start the fan. If e.g [45,50,60,70,80] was "
+# 	"passed to temp_thresholds a stoplevel of 2 will indicate that the "
+# 	"fan has to start when 60deg is reached")
+# @click.option(
+# 	"--fancooldown", type=click.STRING, default="10sek", required=False,
+# 	help=
+# 	"Either a number-string, e.g. '10' which means to cool for another 10 "
+# 	"interval turns after temperature dropping below fanstartlevel or a "
+# 	"time-string, e.g. '10sec' or '10min' means cool for another "
+# 	"10 seconds or 10 minutes"
+# )
+# @click.option(
+# 	"--debug", type=click.BOOL, default=False, required=False,
+# 	help="Print debug messages (1 or 0, true or false)")
+# @click.option(
+# 	"--fan/--nofan", is_flag=True, default=True,
+# 	help="Use the fan (--fan) or don't (--nofan)")
 def run(
 		temp_tresholds, interval, freq, pwmgpio,
 		constant_speed, variable_speeds, fanstartlevel,
@@ -139,7 +141,7 @@ def run(
 
 	try:
 		profile = None
-		config = config_read("config.json")
+		config = config_read("config/config.json")
 		dbg = debug
 
 		if config is not None:
@@ -216,3 +218,8 @@ def run(
 
 if __name__ == "__main__":
 	run()
+	# pid = "/tmp/test.pid"
+
+	# daemon = Daemonize(app="PyFoehn", pid=pid, action=run)
+
+	# daemon.start()
